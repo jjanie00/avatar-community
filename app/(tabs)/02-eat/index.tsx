b00a1b20f2dd0f884f36565c29f8ac32a821b768
jjanie00 <hmyeonge@naver.com>
2025-10-12 08:52:24 +0900

    ✨ Munchie 아이콘 냠냠 메뉴에 렌더링
    아이콘 화면 정중앙 렌더링 및 Log 이동 버튼 위치 조정

--- app/(tabs)/02-eat/index.tsx
+++ app/(tabs)/02-eat/index.tsx
@@ -1,13 +1,36 @@
 import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
-// 아이콘 클릭 > log 화면으로 이동
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
 export default function EatFoodScreen() {
   return (
-    <View>
-      <Link href={"/(tabs)/02-eat/log"}>
-        <Text>Log 화면</Text>
+    <View style={styles.container}>
+      <Link href={"/(tabs)/02-eat/log"} style={styles.link}>
+        <Text>Log 화면 이동</Text>
       </Link>
+      <Image
+        source={require("@/assets/images/munchie.png")}
+        style={styles.image}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  link: {
+    position: "absolute",
+    top: 40,
+    right: 20,
+  },
+  image: {
+    width: width * 0.75,
+    height: height * 0.5,
+  },
+});
