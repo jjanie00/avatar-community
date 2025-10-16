795974961b93bf71380e03376474ad570ba7b644
jjanie00 <hmyeonge@naver.com>
2025-10-16 12:52:06 +0900

    💄 메인 피드 아이템 초기 레이아웃

--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -1,10 +1,27 @@
+import { colors } from "@/constants";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const FeedItem = () => {
   return (
     <View style={styles.container}>
-      <Text>FeedItem</Text>
+      <View style={styles.headerContainer}>
+        <Text>header</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <Text style={styles.description}>게시글 내용</Text>
+      </View>
+      <View style={styles.menuContainer}>
+        <Pressable style={styles.menu}>
+          <Text>menu</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <Text>menu</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <Text>menu</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -14,6 +31,31 @@ export default FeedItem;
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
-    height: 100,
+    padding: 16,
+    gap: 12,
   },
+  headerContainer: {
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  contentContainer: {
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  description: {
+    fontSize: 16,
+    borderWidth: 1,
+    color: "black",
+  },
+  menuContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: colors.GRAY_300,
+    borderWidth: 1,
+  },
+  menu: {},
 });
